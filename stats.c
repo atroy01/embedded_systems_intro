00f41a0714256d36af008f68c3cc4f45e2fa267c
Andrew Troy <atroy01@gmail.com>
2021-04-17 17:15:15 -0600

    Adding code outline and documentation for C1M1 assessment

--- stats.c
+++ stats.c
@@ -8,36 +8,40 @@
  * misuse of this material. 
  *
  *****************************************************************************/
-/**
- * @file <Add File Name> 
- * @brief <Add Brief Description Here >
- *
- * <Add Extended Description Here>
- *
- * @author <Add FirsName LastName>
- * @date <Add date >
- *
- */
 
 
 
-#include <stdio.h>
-#include "stats.h"
+/** 
+* @file stats.c * 
+* @brief This file performs a statistical analysis on an array of numbers. 
+* * * The file contains functions that calculate information such as the mean, mediam, max * 
+* and minimum of an array of numbers and prints the information to the command line. * * 
+* @author Andrew Troy * 
+* @date 3/30/2021 * */ 
 
-/* Size of the Data Set */
-#define SIZE (40)
+#include <stdio.h> 
+#include "stats.h" 
 
-void main() {
+/* Size of the Data Set */ 
+#define SIZE (40) 
 
+void main() { 
+  printf("We have entered main()\n"); 
   unsigned char test[SIZE] = { 34, 201, 190, 154,   8, 194,   2,   6,
                               114, 88,   45,  76, 123,  87,  25,  23,
                               200, 122, 150, 90,   92,  87, 177, 244,
                               201,   6,  12,  60,   8,   2,   5,  67,
-                                7,  87, 250, 230,  99,   3, 100,  90};
+                                7,  87, 250, 230,  99,   3, 100,  90}; 
+  /* Other Variable Declarations Go Here */ 
+  /* Statistics and Printing Functions Go Here */ 
+} 
+    
+/* Add other Implementation File Code Here */ 
 
-  /* Other Variable Declarations Go Here */
-  /* Statistics and Printing Functions Go Here */
-
-}
-
-/* Add other Implementation File Code Here */
+int print_statistics(int mean, int median, int maximum, int minimum){} 
+int print_array(int length, unsigned char arr[]){} 
+int find_median(int length, unsigned char arr[]){}
+int find_maximum(int length, unsigned char arr[]){} 
+int find_minimum(int length, unsigned char arr[]){} 
+int find_mean(int length, unsigned char arr[]){}
+int sort_array(int length, unsigned char arr[]){}
--- stats.h
+++ stats.h
@@ -9,13 +9,13 @@
  *
  *****************************************************************************/
 /**
- * @file <Add File Name> 
- * @brief <Add Brief Description Here >
+ * @file stats.h 
+ * @brief This file contains the function declarations for stats.c.
  *
- * <Add Extended Description Here>
- *
- * @author <Add FirsName LastName>
- * @date <Add date >
+ * These function declarations specify the parameters needed for each function within stats.c
+ * These fucntions are used to display common statistics on a given array of numbers.
+ * @author Andrew Troy
+ * @date 3/30/2021
  *
  */
 #ifndef __STATS_H__
@@ -23,18 +23,91 @@
 
 /* Add Your Declarations and Function Comments here */ 
 
-/**
- * @brief <Add Brief Description of Function Here>
- *
- * <Add Extended Description Here>
- *
- * @param <Add InputName> <add description here>
- * @param <Add InputName> <add description here>
- * @param <Add InputName> <add description here>
- * @param <Add InputName> <add description here>
- *
- * @return <Add Return Informaiton here>
- */
 
+/**
+ * @brief Print statistics of an array
+ *
+ * This function prints various statistcs to the screen
+ *
+ * @param mean The mean of the array
+ * @param median The median of the array
+ * @param maximum The maximum of the array
+ * @param minimum The minimum of the array
+ *
+ * @return Nothing. Just print the statistics to the screen using printf();
+ */
+int print_statistics(int mean, int median, int maximum, int minimum);
+
+/**
+ * @brief Print an array to the screen
+ *
+ * This function takes as input an array and a length and prints the array to the screen
+ *
+ * @param array The array of numbers to print to the screen
+ * @param length The length of the array
+ *
+ * @return Nothing. Just print the array to the screen using printf();
+ */
+int print_array(int length, unsigned char arr[]);
+
+/**
+ * @brief Find the median of an array
+ *
+ * This function takes as input an array and a length and returns the median of the array as an int
+ *
+ * @param array The array of numbers from which the median will be calculated
+ * @param length The length of the array
+ *
+ * @return The median of the array
+ */
+int find_median(int length, unsigned char arr[]);
+
+/**
+ * @brief Find the mean of an array
+ *
+ * This function takes as input an array and a length and returns the mean of the array as an int
+ *
+ * @param array The array of numbers from which the mean will be calculated
+ * @param length The length of the array
+ *
+ * @return The mean of the array
+ */
+int find_mean(int length, unsigned char arr[]);
+
+/**
+ * @brief Find the maximum of an array
+ *
+ * This function takes as input an array and a length and returns the maximum of the array as an int
+ *
+ * @param array The array of numbers from which the maximum will be identified
+ * @param length The length of the array
+ *
+ * @return The maximum of the array
+ */
+int find_maximum(int length, unsigned char arr[]);
+
+/**
+ * @brief Find the minimum of an array
+ *
+ * This function takes as input an array and a length and returns the minimum of the array as an int
+ *
+ * @param array The array of numbers from which the minimum will be identified
+ * @param length The length of the array
+ *
+ * @return The minimum of the array
+ */
+int find_minimum(int length, unsigned char arr[]);
+
+/**
+ * @brief Sort an array
+ *
+ * This function takes as input an array and a length sorts the array from largest to smallest and prints sorted array to the screen
+ * 
+ * @param array The array of numbers to be sorted
+ * @param length The length of the array
+ *
+ * @return Nothing. Print to sorted array to the screen.
+ */
+int sort_array(int length, unsigned char arr[]);
 
 #endif /* __STATS_H__ */
